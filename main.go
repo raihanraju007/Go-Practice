b32e8c8c4840822117d37ece349327b599f192ec
raihanraju007 <raihanraju007@gmail.com>
2024-08-18 16:03:13 +0600

    variables practise

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World From Go")
+	// fmt.Println("Hello World From Go")
+	var fullName string;
+	var age int;
+	var gpa float32;
+	var country string;
+
+	fullName = "Md Raihan Hossin";
+	age = 30;
+	gpa = 3.47;
+	country = "Bangladesh";
+
+	fmt.Println(fullName, "is a student");
+	fmt.Println(fullName, "is", age, "years old");
+	fmt.Println(fullName, "has got", gpa,"/4 in bsc");
+	fmt.Println(fullName, " is originally from", country);
+
 }
