9f139fa6afac6db43b302cd46483c45cfa9e5776
raihanraju007 <raihanraju007@gmail.com>
2024-08-19 17:44:28 +0600

    number and string formatting

--- user_input.go
+++ user_input.go
@@ -4,22 +4,34 @@ import "fmt"
 
 func main(){
 
-	var fullName string;
-	var number1, number2 int;
+	// var fullName string;
+	// var number1, number2 int;
 
-	// Input : Scan, Scanln, Scanfageage
-	// Output : Print, Println, Printf
+	// // Input : Scan, Scanln, Scanfageage
+	// // Output : Print, Println, Printf
 
-	fmt.Print("Enter your name: ")
-	fmt.Scan(&fullName);
+	// fmt.Print("Enter your name: ")
+	// fmt.Scan(&fullName);
 	
 
-	fmt.Print("Enter Two Numbers: ");
-	fmt.Scan(&number1, &number2);
-	// fmt.Scanf("%v %v", &number1, &number2);
+	// fmt.Print("Enter Two Numbers: ");
+	// fmt.Scan(&number1, &number2);
+	// // fmt.Scanf("%v %v", &number1, &number2);
 
-	fmt.Printf("Full name is :  %v\n",fullName);
-	fmt.Printf("number1 = %v, number2 = %v\n",number1,number2);
+	// fmt.Printf("Full name is :  %v\n",fullName);
+	// fmt.Printf("number1 = %v, number2 = %v\n",number1,number2);
+
+	var number = 3.1415;
+	fmt.Printf("%.2f\n", number);
+
+	var decimalNumber int;
+	fmt.Printf("Decimal Number: ")
+	fmt.Scanf("%v",&decimalNumber)
+
+	fmt.Printf("Binary Number 	= %b\n",&decimalNumber)
+	fmt.Printf("Octal Number 	= %o\n",&decimalNumber)
+	fmt.Printf("Hexa Number 	= %x\n",&decimalNumber)
+
 
 
 }
