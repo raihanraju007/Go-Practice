953b78176818f5117c050737651ae7f82741af1c
raihanraju007 <raihanraju007@gmail.com>
2024-08-21 15:41:41 +0600

    arithmetic operator

---
+++ arithmetic_operator.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+
+	fmt.Printf("Num1 = ")
+	fmt.Scan(&num1)
+
+	fmt.Printf("Num2 = ")
+	fmt.Scan(&num2)
+
+	result := num1 + num2
+	fmt.Printf("%v + %v = %v\n", num1, num2, result)
+}
