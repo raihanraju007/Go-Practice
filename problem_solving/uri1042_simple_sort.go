8fffbbe6236a179bb07c0838e6c4ffa3e53754f8
raihanraju007 <raihanraju007@gmail.com>
2024-09-11 15:16:32 +0600

    uri1042 problem

---
+++ problem_solving/uri1042_simple_sort.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("beecrowd | 1042 Simple Sort")
+
+	var a int
+	var b int
+	var c int
+	
+
+	fmt.Scan(&a, &b, &c) 
+
+	original := [] int{a, b, c}
+
+	sorted :=make([]int,3)
+	copy(sorted,original) 
+
+	sort.Ints(sorted)
+
+
+	for _, i := range sorted {
+		fmt.Println(i)
+	}
+
+	fmt.Println()
+
+	for _, v := range original {
+		fmt.Println(v)
+	}
+	
+}
